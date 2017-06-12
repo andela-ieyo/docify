da014426f0d6389ed53596c628bdf352dbe3d05f
Eyo <ifiokabasi.eyo@andela.com>
2017-06-12 19:41:39 +0100

    add build and postinstall scripts

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
-import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import BabiliPlugin from 'babili-webpack-plugin';
-import path from 'path';
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BabiliPlugin = require('babili-webpack-plugin');
 
 const BUILD_PATH = path.resolve(__dirname, 'public');
 const APP_DIR = `${path.resolve(__dirname)}/app`;
