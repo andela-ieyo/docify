64deae92b4da7d58c120f9ed5c79af6478c7fef4
Eyo <ifiokabasi.eyo@andela.com>
2017-06-23 19:55:42 +0100

    add travis db config

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,9 @@ module.exports = {
     port: 5432,
     dialect: 'postgres'
   },
+  travis: {
+    use_env_variable: 'DATABASE_URL'
+  },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
